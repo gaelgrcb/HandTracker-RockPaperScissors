882e11ed5f40c023bb5c6c7c8a6da05db7cd771b
Gael Garcia <gaelgarciabst@gmail.com>
2024-11-28 14:22:37 -0700

    Finish game logic

--- Rock-Paper-Scissors.py
+++ Rock-Paper-Scissors.py
@@ -1,13 +1,12 @@
 import cv2
 import mediapipe as mp
 import numpy as np
-
-#Video Capture
-camera = cv2.VideoCapture(0)
+import random
 
 #Init MP and CV
-mpHands = mp.solutions.hands
-mpDraw = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
+mp_drawing = mp.solutions.drawing_utils
 
 #hands = mpHands.Hands(static_image_mode= False,
 #                        min_detection_confidence= 0.9,
@@ -19,6 +18,28 @@ def gestures(hand_landmarks):
     index_tip = hand_landmarks.landmark[8]
     middle_tip = hand_landmarks.landmark[12]
 
+    #Calculate the distance between finger an middle finger
+    distance = ((index_tip - middle_tip) ** 2 + (index_tip.y - middle_tip.y) ** 2) * 0.5
+
+    if distance < 0.05:
+        return "Tijera"
+    elif index_tip < middle_tip.y:
+        return "Papel"
+    else:
+        return "Piedra"
+
+#Function to determinate the winner
+def winner(player_choice, computer_choice):
+    if player_choice == computer_choice:
+        return "Empate"
+    #Win combinations
+    elif (player_choice == "Piedra" and computer_choice == "Tijera") or (player_choice == "Tijera" and computer_choice == "Papel") or (player_choice == "Papel" and computer_choice == "Piedra"):
+        return "Ganaste"
+    else:
+        return "Perdiste"
+
+#Video Capture
+camera = cv2.VideoCapture(0)
 while True:
     success,img = camera.read()
 
@@ -30,9 +51,9 @@ while True:
 
     if result.multi_hand_landmarks:
         for handLm in result.multi_hand_landmarks:
-            mpDraw.draw_landmarks(img, handLm, mpHands.HAND_CONNECTIONS)
+            mp_drawing.draw_landmarks(img, handLm, mp_hands.HAND_CONNECTIONS)
 
-    cv2.imshow("Hand Detection", img)
+    cv2.imshow("Hand Tracker", img)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
