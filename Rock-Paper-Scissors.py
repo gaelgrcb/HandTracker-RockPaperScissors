8f62ebd649a8e90aeb37a24305f5642cb9b59edc
Gael Garcia <gaelgarciabst@gmail.com>
2024-11-28 03:05:55 -0700

    Configure hand movement

--- Rock-Paper-Scissors.py
+++ Rock-Paper-Scissors.py
@@ -1,2 +1,11 @@
 import cv2
 import mediapipe as mp
+import numpy as np
+
+camera = cv2.VideoCapture(0)
+mpHands = mp.solutions.hands
+mpHands = mpHands.Hands(satic_image_mode = False,
+                        min_detection_confidence= 0.9,
+                        min_tracking_confidence= 0.8)
+mpDraw = mp.solutions.drawing_utils
+
