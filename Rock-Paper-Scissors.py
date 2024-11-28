8b2a81773fa92ed18b1227239fe0a7bea8bcf108
Gael Garcia <gaelgarciabst@gmail.com>
2024-11-28 03:09:55 -0700

    Variable name correction

--- Rock-Paper-Scissors.py
+++ Rock-Paper-Scissors.py
@@ -4,8 +4,14 @@ import numpy as np
 
 camera = cv2.VideoCapture(0)
 mpHands = mp.solutions.hands
-mpHands = mpHands.Hands(satic_image_mode = False,
+
+hands = mpHands.Hands(satic_image_mode = False,
                         min_detection_confidence= 0.9,
                         min_tracking_confidence= 0.8)
 mpDraw = mp.solutions.drawing_utils
 
+while True:
+    success,img = camera.read()
+
+    imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    result = hands.process(imgRGB)
